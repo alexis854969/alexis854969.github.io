f033d280a45a107fe0765fbde8833d94e039109b
alexis854969 <a-auriou@stemarieduport.fr>
2022-05-20 11:52:51 +0200

    initialisation projet

---
+++ accueil.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8" />
+     <link rel="stylesheet" href="style 10.css" /><!--
+            ne pas oublier de changerle nom de la feuille de style
+        -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+</head>
+
+<body>
+    <?php include('nav.php'); ?>
+        <article>
+           <fieldset>
+                <h3>titre du paragraphe</h3>
+                <p>Ici ma dernière journée</p>
+
+                <a href="ajout.php"><button>Ajouter</button></a>
+                </fieldset>
+        </article>
+        <article>
+           <fieldset>
+                <h3>titre du paragraphe</h3>
+                <p>ici Description projet</p>
+            </fieldset>
+        </article>        
+        <article>
+           <fieldset>
+                <h3>Presentation de l'étudiant</h3>
+                <p>Je m'appele alexis Auriou , j'ai 17 ans , éleves a saint-marie du port en premier SN en profesionel </p>
+                <img src="" alt="ma photo">
+            </fieldset>
+        </article>    
+</body>
+</html>
